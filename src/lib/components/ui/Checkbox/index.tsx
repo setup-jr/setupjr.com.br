cafd55669fa3bb24537130c0149e09e1f1365d3e
Henrique GC <henriq4net@gmail.com>
2023-05-25 14:50:08 -0300

    feat: create components ui - button, checkbox and input

---
+++ src/lib/components/ui/Checkbox/index.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { HTMLAttributes } from "react";
+
+type CheckboxProps = HTMLAttributes<HTMLInputElement>;
+
+const Checkbox = ({ ...rest }: CheckboxProps) => (
+  <input
+    {...rest}
+    type="checkbox"
+    className={`${rest.className || ""} form-checkbox duration-75 rounded`}
+  />
+);
+
+export default Checkbox;
