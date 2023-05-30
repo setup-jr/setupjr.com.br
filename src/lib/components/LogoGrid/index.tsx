82fff2695e207afeae262ace7b43a71eed910ce0
Henrique GC <henriq4net@gmail.com>
2023-05-30 07:40:16 -0300

    feat: create logos component

---
+++ src/lib/components/LogoGrid/index.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import { logos } from "./logos";
+
+const LogoGrid = () => (
+  <div>
+    <div className="custom-screen">
+      <h2 className="font-semibold text-sm text-gray-600 text-center">
+        CONFIADO POR EQUIPES DO MUNDO TODO.
+      </h2>
+      <div className="mt-6">
+        <ul className="flex gap-x-10 gap-y-6 flex-wrap items-center justify-center md:gap-x-16">
+          {logos.map((item, idx) => (
+            <li key={idx}>
+              <Image src={item.src} alt={item.alt} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  </div>
+);
+
+export default LogoGrid;
