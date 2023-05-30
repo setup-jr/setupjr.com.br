c9a42bf6163c74ae62349154edca744efb99abf2
Henrique GC <henriq4net@gmail.com>
2023-05-30 07:42:32 -0300

    feat: create main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,25 @@
 import "lib/styles/global.css";
 
-import Footer from "lib/components/Footer";
-import Navbar from "lib/components/Navbar";
-// import { Inter } from "next/font/google";
+import { Jost } from "next/font/google";
 import { Metadata } from "next";
 
-// const inter = Inter({ subsets: ["latin"] });
+import Footer from "lib/components/Footer";
+import Navbar from "lib/components/Navbar";
+
+const jost = Jost({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Setup JR",
   description: "Setup JR official website",
   viewport: "width=device-width, initial-scale=1",
   creator: "Setup JR",
-  authors: [{ name: "Henrique GC" }],
+  authors: [
+    { name: "Henrique GC" },
+    { name: "Gustavo Denobi" },
+    { name: "Gabriel Lima" },
+    { name: "Felipe Ferreira" },
+    { name: "Leonardo Faria" },
+  ],
 };
 
 export default function RootLayout({
@@ -26,7 +33,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head> */}
 
-      <body className="md:overflow-visible">
+      <body className={`md:overflow-visible ${jost.className}`}>
         <Navbar />
         <main>{children}</main>
         <Footer />
